ede92cd6b64857466f32acbefb1ae9cc0f9d3083
Morgan Wulff <Mogge_Wulff@hotmail.com>
2023-01-30 18:03:21 +0100

    Create components from other template (navigation not set up yet)

--- WebUI/src/app/app.component.ts
+++ WebUI/src/app/app.component.ts
@@ -1,26 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
-
-  constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
-
-  title = 'WebUI';
-}
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+  title = 'app';
 }
--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { CounterComponent } from './counter/counter.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FetchDataComponent,
+    NavMenuComponent,
+    CounterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, NgbModule
