ca79d39f69f6ad3fb012759a5c85b506e74c6810
Morgan Wulff <Mogge_Wulff@hotmail.com>
2023-02-05 18:59:12 +0100

    Remove template/example components

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -4,10 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { CounterComponent } from './counter/counter.component';
-import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
 import { SearchSynonymsComponent } from './search-synonyms/search-synonyms.component';
 import { SearchComponent } from './search/search.component';
@@ -15,20 +12,14 @@ import { SearchComponent } from './search/search.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FetchDataComponent,
     NavMenuComponent,
-    CounterComponent,
-    HomeComponent,
     SearchSynonymsComponent,
     SearchComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, NgbModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'search', component: SearchSynonymsComponent }
+      { path: '', component: SearchSynonymsComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
