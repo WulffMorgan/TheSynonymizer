e3d2c293f7182a49b06c0caeb85ffbaeb40b4fc4
Morgan Wulff <Mogge_Wulff@hotmail.com>
2023-02-05 17:36:11 +0100

    Implement GUI for searching words

--- WebUI/src/app/search-synonyms/search-synonyms.component.ts
+++ WebUI/src/app/search-synonyms/search-synonyms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-synonyms',
@@ -7,8 +8,27 @@ import { Component } from '@angular/core';
 })
 export class SearchSynonymsComponent {
 
-  public onSearch(searchValue: string): void {
-    console.log(searchValue);
+  private http: HttpClient;
+  private _wordWithSynonyms?: WordWithSynonyms;
+
+  public constructor(http: HttpClient) {
+    this.http = http;
   }
 
+  public onSearch(searchValue: string): void {
+    if (!searchValue)
+      return;
+
+    this.http.get<WordWithSynonyms>(`/api/synonyms/${searchValue}`).subscribe(result => {
+      this._wordWithSynonyms = result;
+    }, error => console.error(error));
+  }
+
+  get wordWithSynonyms(): WordWithSynonyms | undefined { return this._wordWithSynonyms }
+
+}
+
+interface WordWithSynonyms {
+  word: string;
+  synonyms: string[];
 }
