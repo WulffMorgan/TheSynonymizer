285b2a66bb7cfec55e70efdb9e3c50f9879f8ba6
Morgan Wulff <Mogge_Wulff@hotmail.com>
2023-02-12 22:28:56 +0100

    Add a footer with links to GitHub repo and the OpenAPI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,11 +8,8 @@ builder.Services.AddServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
-{
-    _=app.UseSwagger();
-    _=app.UseSwaggerUI();
-}
+_=app.UseSwagger();
+_=app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
