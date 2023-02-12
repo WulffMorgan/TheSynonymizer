a0416c24394a359468c787b7b4fc664091a0d6af
Morgan Wulff <Mogge_Wulff@hotmail.com>
2023-02-12 15:52:03 +0100

    Collapse nav-menu on router navigation end

--- WebUI/src/app/nav-menu/nav-menu.component.ts
+++ WebUI/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.scss']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit {
 
-  collapse() {
-    this.isExpanded = false;
+  private _isExpanded = false;
+  private _router: Router;
+
+  public constructor(router: Router) {
+    this._router = router;
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  public get isExpanded() { return this._isExpanded; }
+
+  public ngOnInit(): void {
+    this._router.events
+      .pipe(filter(evt => evt instanceof NavigationEnd))
+      .subscribe(_evt => this.collapse());
+  }
+
+  public collapse() {
+    this._isExpanded = false;
+  }
+
+  public toggle() {
+    this._isExpanded = !this._isExpanded;
   }
 }
